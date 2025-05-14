9bf94d8a7ad59e49d74ca11eb054a18e3b551c1f
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-14 15:26:42 +0900

    a

--- db/db_kubota_select_14.sql
+++ db/db_kubota_select_14.sql
@@ -1,18 +1,18 @@
 /* ‰Û‘è‡A */
 /* Q.14 */
 SELECT
-	car_class_id,
-	price_kbn,
-	option_id,
-	lending_time_kbn,
-	price,
-	ins_date,
-	ins_user_id,
-	upd_counter,
-	upd_date,
-	upd_user_id
+    car_class_id,
+    price_kbn,
+    option_id,
+    lending_time_kbn,
+    price,
+    ins_date,
+    ins_user_id,
+    upd_counter,
+    upd_date,
+    upd_user_id
 FROM
-	nmrm_price_structure
+    nmrm_price_structure
 WHERE
-		car_class_id = 'SC-1'
-	AND lending_time_kbn = 0003;
+        car_class_id = 'SC-1'
+    AND lending_time_kbn = 0003;
