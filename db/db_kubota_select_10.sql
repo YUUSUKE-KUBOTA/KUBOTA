db9d99e99faa5cf607a038c499de529269dc2ea5
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-14 15:22:37 +0900

    a

--- db/db_kubota_select_10.sql
+++ db/db_kubota_select_10.sql
@@ -1,14 +1,14 @@
 /* ‰Û‘è‡A */
 /* Q.10 */
 SELECT
-	shop_id,
-	shop_name,
-	ins_date,
-	ins_user_id,
-	upd_counter,
-	upd_date,
-	upd_user_id
+    shop_id,
+    shop_name,
+    ins_date,
+    ins_user_id,
+    upd_counter,
+    upd_date,
+    upd_user_id
 FROM
-	nmrm_shop
+    nmrm_shop
 WHERE
-	shop_name LIKE '%”L%';
+    shop_name LIKE '%”L%';
