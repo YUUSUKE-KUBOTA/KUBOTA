0e8b42baf8424f64a7e452eea1f74e75059a9d9c
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-14 15:17:51 +0900

    a

--- db/db_kubota_select_06.sql
+++ db/db_kubota_select_06.sql
@@ -1,9 +1,9 @@
 /* ‰Û‘è‡A */
 /* Q.06 */
 SELECT
-	car_model_name
+    car_model_name
 FROM
-	nmrm_car_model
+    nmrm_car_model
 WHERE
-		fuel_economy >= 20.0
-	AND fuel_economy <= 29.9;
+        fuel_economy >= 20.0
+    AND fuel_economy <= 29.9;
