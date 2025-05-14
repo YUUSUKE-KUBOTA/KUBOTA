056b7dbf48477e4fc08cf86a4c5cb81c75944124
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-14 15:21:01 +0900

    a

--- db/db_kubota_select_09.sql
+++ db/db_kubota_select_09.sql
@@ -1,20 +1,20 @@
 /* ‰Û‘è‡A */
 /* Q.09 */
 SELECT
-	car_model_id,
-	car_model_name,
-	car_type_kbn,
-	car_class_id,
-	maker_id,
-	capacity,
-	fuel_economy,
-	displacement,
-	ins_date,
-	ins_user_id,
-	upd_counter,
-	upd_date,
-	upd_user_id
+    car_model_id,
+    car_model_name,
+    car_type_kbn,
+    car_class_id,
+    maker_id,
+    capacity,
+    fuel_economy,
+    displacement,
+    ins_date,
+    ins_user_id,
+    upd_counter,
+    upd_date,
+    upd_user_id
 FROM
-	nmrm_car_model
+    nmrm_car_model
 WHERE
-	car_model_name LIKE '%ƒƒSƒ“%';
+    car_model_name LIKE '%ƒƒSƒ“%';
