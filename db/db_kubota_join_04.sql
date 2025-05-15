0a5aadc446322faec44e4c1e707b976292ab4355
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-15 15:48:30 +0900

    a

--- db/db_kubota_join_04.sql
+++ db/db_kubota_join_04.sql
@@ -8,7 +8,8 @@ SELECT
 FROM
     nmrm_rent_car
     INNER JOIN nmrm_shop ON (nmrm_rent_car.shop_id = nmrm_shop.shop_id)
-    INNER JOIN nmrt_lending_status ON (nmrm_rent_car.car_management_no = nmrt_lending_status.car_management_no)
+    LEFT OUTER JOIN nmrt_lending_status ON (nmrm_rent_car.car_management_no = nmrt_lending_status.car_management_no)
     INNER JOIN nmrm_car_model ON (nmrm_rent_car.car_model_id = nmrm_car_model.car_model_id)
 WHERE
-    shop_name = '”LƒoƒXŽæˆµ“X';
+        shop_name = '”LƒoƒXŽæˆµ“X'
+	AND nmrt_lending_status.car_management_no IS NULL;
