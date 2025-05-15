f0bbfb1b1f278b2261576c5e3c0e0d9442418935
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-15 09:40:36 +0900

    a

--- db/db_kubota_insert_01.sql
+++ db/db_kubota_insert_01.sql
@@ -1,29 +1,17 @@
 /* ‰Û‘è‡D */
 /* Q.01 */
 INSERT INTO 
-	nmrm_rent_car (	
-		car_management_no,
-		car_model_id,
-		shop_id,
-		etc,
-		car_navigation,
-		smoking_kbn,
-		ins_date,
-		ins_user_id ,
-		upd_counter,
-		upd_date,
-		upd_user_id
-	)
-VALUES
-	(
-		'100101',
-		'000001',
-		'000005',
-		'1',
-		'1',
-		'2',
-		'2025-5-1414:00:00',
-		'000002',
-		'2025-5-1414:00:00',
-		'000002',
-		'0');
+	nmrm_rent_car
+VALUES (
+    '100101',
+    '000001',
+    '000005',
+    '1',
+    '1',
+    '2',
+    '2025-05-15 14:00:00',
+    '000002',
+    0,
+    '2025-05-15 14:00:00',
+    '000002'
+);
---
+++ db/db_kubota_insert_02.sql
@@ -0,0 +1,22 @@
+/* 課題⑤ */
+/* Q.02 */
+INSERT INTO nmrm_option (
+    option_id,
+    option_name,
+    option_price,
+    ins_date,
+    ins_user_id,
+    upd_counter,
+    upd_date,
+    upd_user_id
+)
+VALUES (
+    '0001',
+	'チャイルドシート',
+	0,
+	'2025-05-15 14:00:00',
+	'000002',
+	0,
+	'2025-05-15 14:00:00',
+	'000002'
+);
