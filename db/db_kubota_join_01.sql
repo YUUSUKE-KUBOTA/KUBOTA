daafd0b72e84ca894ba89500e8e837f1e8c9e784
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-15 14:38:04 +0900

    a

---
+++ db/db_kubota_join_01.sql
@@ -0,0 +1,9 @@
+SELECT
+    car_management_no,
+    car_model_name
+FROM
+    nmrm_rent_car
+    INNER JOIN nmrm_shop ON (nmrm_rent_car.shop_id = nmrm_shop.shop_id)
+    INNER JOIN nmrm_car_model ON (nmrm_rent_car.car_model_id = nmrm_car_model.car_model_id)
+WHERE
+    shop_name = '‚É‚á‚ñ‚±“X';
