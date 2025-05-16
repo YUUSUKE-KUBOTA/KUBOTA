eb6be6e2f4df79cbc0c2071177e9003253764ca0
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-16 15:51:34 +0900

    a

--- db/db_kubota_exercise_35.sql
+++ db/db_kubota_exercise_35.sql
@@ -2,7 +2,7 @@
 SELECT
     position,
     (SELECT MAX(height)) as max_height,
-	(SELECT TOP 1 name FROM players WHERE position = player.position ORDER BY height DESC) AS player_name
+    (SELECT TOP 1 name FROM players WHERE position = player.position ORDER BY height DESC) AS player_name
 FROM
     players AS player
 WHERE
