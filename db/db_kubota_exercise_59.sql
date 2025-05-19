78f201c69527be6a6bf2c7701be31c4ed12648b6
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 17:19:28 +0900

    a

--- db/db_kubota_exercise_59.sql
+++ db/db_kubota_exercise_59.sql
@@ -4,19 +4,19 @@ SELECT
     my_country.name AS my_country_name,
     enemy_country.name AS enemy_country_name,
     my_country.ranking AS my_country_ranking,
-    enemy_country.ranking AS enemy_country_ranking
---	(SELECT 
---	    COUNT(goals.id) 
---	 FROM
---	    goals 
---	 WHERE
---	    goals.pairing_id = pairings.id AND goals.player_id IN
---	                            (SELECT 
---								   players.id 
---								 FROM 
---								   players 
---								 WHERE 
---								   players.country_id = pairings.my_country_id)) AS my_country_goals
+    enemy_country.ranking AS enemy_country_ranking,
+	(SELECT 
+	    COUNT(goals.id) 
+	 FROM
+	    goals 
+	 WHERE
+	    goals.pairing_id = pairings.id AND goals.player_id IN
+	                            (SELECT 
+								   players.id 
+							     FROM 
+							       players 
+								 WHERE 
+								   players.country_id = pairings.my_country_id)) AS my_country_goals
 FROM
     pairings
     INNER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
