ccdca7c31419691c01c1461c00f82ea367510240
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 15:56:47 +0900

    a

--- db/db_kubota_exercise_59.sql
+++ db/db_kubota_exercise_59.sql
@@ -1,15 +1,16 @@
 /* í«â¡ââèK59 */
 SELECT
-    pairings.kickoff,
+    DISTINCT pairings.kickoff,
     my_country.name AS my_country_name,
     enemy_country.name AS enemy_country_name,
     my_country.ranking AS my_country_ranking,
     enemy_country.ranking AS enemy_country_ranking,
-    (SELECT )
+	(SELECT COUNT(goals.id) FROM goals WHERE goals.pairing_id = pairings.id AND goals.player_id IN
+	  (SELECT players.id FROM players WHERE players.country_id = pairings.my_country_id)) AS my_country_goals
 FROM
     pairings
-    LEFT OUTER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
-    LEFT OUTER JOIN countries AS enemy_country ON (pairings.enemy_country_id = enemy_country.id)
+    INNER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
+    INNER JOIN countries AS enemy_country ON (pairings.enemy_country_id = enemy_country.id)
 WHERE
     my_country.group_name = 'C'
     AND enemy_country.group_name = 'C'
---
+++ db/db_kubota_exercise_60.sql
@@ -0,0 +1,21 @@
+/* í«â¡ââèK60 */
+SELECT
+    DISTINCT pairings.kickoff,
+    my_country.name AS my_country_name,
+    enemy_country.name AS enemy_country_name,
+    my_country.ranking AS my_country_ranking,
+    enemy_country.ranking AS enemy_country_ranking,
+	(SELECT COUNT(goals.id) FROM goals WHERE goals.pairing_id = pairings.id AND goals.player_id IN
+	  (SELECT players.id FROM players WHERE players.country_id = pairings.my_country_id)) AS my_country_goals,
+	(SELECT COUNT(goals.id) FROM goals WHERE goals.pairing_id = pairings.id AND goals.player_id IN
+	  (SELECT players.id FROM players WHERE players.country_id = pairings.enemy_country_id)) AS enemy_country_goals
+FROM
+    pairings
+    INNER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
+    INNER JOIN countries AS enemy_country ON (pairings.enemy_country_id = enemy_country.id)
+WHERE
+    my_country.group_name = 'C'
+    AND enemy_country.group_name = 'C'
+ORDER BY
+    pairings.kickoff,
+    my_country.ranking;
