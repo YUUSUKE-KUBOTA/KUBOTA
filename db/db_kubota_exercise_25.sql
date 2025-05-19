a1087a8710ec66a7ab12390a8900cbbf619289ee
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 11:46:38 +0900

    a

--- db/db_kubota_exercise_25.sql
+++ db/db_kubota_exercise_25.sql
@@ -1,6 +1,6 @@
 /* ’Ç‰Á‰‰K25 */
 SELECT
-    SUBSTRING(CONVERT(varchar, birth, 112), 1, 4) + '”N' + SUBSTRING(CONVERT(varchar, birth, 112), 5, 2) + 'Œ' + SUBSTRING(CONVERT(varchar, birth, 112), 7, 2) + '“ú'
+    FORMAT(birth, 'yyyy”NMMŒdd“ú') AS birth
 FROM
     players
 WHERE
