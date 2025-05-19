44bd1b37c2d771e2001c05051ab9d5ac128962ea
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 16:34:39 +0900

    a

--- db/db_kubota_exercise_57.sql
+++ db/db_kubota_exercise_57.sql
@@ -1,6 +1,26 @@
 /* 追加演習57 */
 SELECT
     CASE
-	  WHEN (SELECT  FROM pairings WHERE pairings.id = 39) > (SELECT  FROM pairings WHERE pairings.id = 103) THEN 'win:日本'
-	  WHEN (SELECT  FROM pairings WHERE pairings.id = 39) < (SELECT  FROM pairings WHERE pairings.id = 103) THEN 'win:コロンビア'
-	END
+        WHEN japan_goals > colombia_goals THEN 'win:日本 lose:コロンビア'
+        WHEN japan_goals < colombia_goals THEN 'win:コロンビア lose:日本'
+        ELSE '引き分け'
+    END AS result
+FROM (
+    SELECT
+        (SELECT
+           COUNT(goals.id)
+         FROM 
+           pairings
+           INNER JOIN goals ON pairings.id = goals.pairing_id
+           INNER JOIN players ON goals.player_id = players.id
+         WHERE
+           pairing_id = 39) AS japan_goals,
+        (SELECT
+           COUNT(goals.id)
+         FROM
+           pairings
+           INNER JOIN goals ON pairings.id = goals.pairing_id
+           INNER JOIN players ON goals.player_id = players.id
+         WHERE
+           pairing_id = 103) AS colombia_goals
+) AS scores;
