592ed952c89ee952b594bf442422578fb5c3004b
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 17:16:56 +0900

    a

--- db/db_kubota_exercise_55.sql
+++ db/db_kubota_exercise_55.sql
@@ -4,8 +4,8 @@ SELECT
     COUNT(DISTINCT goals.id) AS group_goals
 FROM
     pairings
-	LEFT OUTER JOIN countries ON pairings.my_country_id = countries.id
-	LEFT OUTER JOIN goals ON goals.pairing_id = pairings.id
+    LEFT OUTER JOIN countries ON pairings.my_country_id = countries.id
+    LEFT OUTER JOIN goals ON goals.pairing_id = pairings.id
 WHERE
     pairings.kickoff BETWEEN '2014-06-13' AND '2014-06-27'
 GROUP BY
