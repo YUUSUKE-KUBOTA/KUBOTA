fabae2f1a1cfa022f4cf856a4f7b78248d5846e8
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-19 15:27:02 +0900

    a

--- db/db_kubota_exercise_58.sql
+++ db/db_kubota_exercise_58.sql
@@ -1,19 +1,26 @@
 /* í«â¡ââèK57 */
 SELECT
     pairings.kickoff,
-    my_country.name,
-    enemy_country.name,
-    my_country.ranking,
-    enemy_country.ranking,
-	COUNT(goals.player_id)
+    my_country.name AS my_country_name,
+    enemy_country.name AS enemy_country_name,
+    my_country.ranking AS my_country_ranking,
+    enemy_country.ranking AS enemy_country_ranking,
+    COUNT(goals.id) AS my_country_goals
 FROM
     pairings
     LEFT OUTER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
     LEFT OUTER JOIN countries AS enemy_country ON (pairings.enemy_country_id = enemy_country.id)
 	LEFT OUTER JOIN players ON (my_country.id = players.country_id)
-	LEFT OUTER JOIN goals ON (players.id = goals.player_id)
+	LEFT OUTER JOIN goals ON (players.id = goals.player_id AND goals.pairing_id = pairings.id)
 WHERE
     my_country.group_name = 'C'
-	AND enemy_country.group_name = 'C'
+    AND enemy_country.group_name = 'C'
 GROUP BY
-    
+    pairings.kickoff,
+    my_country.name,
+    enemy_country.name,
+    my_country.ranking,
+    enemy_country.ranking
+ORDER BY
+    pairings.kickoff,
+    my_country.ranking;
