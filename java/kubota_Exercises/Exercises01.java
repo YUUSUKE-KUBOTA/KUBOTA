99f2208ac168d906b958877b3ce555c6af461b7e
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-20 16:46:27 +0900

    a

---
+++ java/kubota_Exercises/Exercises01.java
@@ -0,0 +1,10 @@
+package jp.co.scsk.kyushu.conditionalBranch;
+
+public class Exercises01 {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ java/kubota_Exercises/Exercises02.java
@@ -0,0 +1,16 @@
+package jp.co.scsk.kyushu.conditionalBranch;
+
+public class Exercises02 {
+	public static void main(String[] args) {
+		int age = 10;
+		int fee;
+		if (age >= 0 || age <=2) {
+			fee = 0;
+		} else if (age >= 3 && age <=12) {
+			fee = 4000;
+		} else {
+			fee = 8000;
+		}
+		System.out.println("宿泊料金は" + fee + "円です");
+	}
+}
