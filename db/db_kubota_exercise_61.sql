8acbda3297b7453192169f8bcb46faba6f44dc98
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-20 10:18:40 +0900

    a

---
+++ db/db_kubota_exercise_61.sql
@@ -0,0 +1,66 @@
+/* í«â¡ââèK61 */
+SELECT
+    pairings.kickoff,
+    my_country.name AS my_country_name,
+    enemy_country.name AS enemy_country_name,
+    my_country.ranking AS my_country_ranking,
+    enemy_country.ranking AS enemy_country_ranking,
+    (SELECT
+        COUNT(goals.id) AS my_country_goal
+     FROM
+        goals
+     WHERE
+        goals.player_id IN
+              (SELECT
+                  players.id
+               FROM
+                  players
+               WHERE
+                  players.country_id = pairings.my_country_id)) AS my_country_goals,
+     (SELECT
+        COUNT(goals.id) AS enemy_country_goal
+     FROM
+        goals
+     WHERE
+        goals.player_id IN
+              (SELECT
+                  players.id
+               FROM
+                  players
+               WHERE
+                  players.country_id = pairings.enemy_country_id)) AS enemy_country_goals,
+    (SELECT (SELECT
+        COUNT(goals.id) AS my_country_goal
+     FROM
+        goals
+     WHERE
+        goals.player_id IN
+              (SELECT
+                  players.id
+               FROM
+                  players
+               WHERE
+                  players.country_id = pairings.my_country_id))
+    -
+    (SELECT
+        COUNT(goals.id) AS enemy_country_goal
+     FROM
+        goals
+     WHERE
+        goals.player_id IN
+              (SELECT
+                  players.id
+               FROM
+                  players
+               WHERE
+                  players.country_id = pairings.enemy_country_id))) AS goal_difference
+FROM
+    pairings
+    INNER JOIN countries AS my_country ON (pairings.my_country_id = my_country.id)
+    INNER JOIN countries AS enemy_country ON (pairings.enemy_country_id = enemy_country.id)
+WHERE
+    my_country.group_name = 'C'
+    AND enemy_country.group_name = 'C'
+ORDER BY
+    pairings.kickoff,
+    my_country.ranking;
