2ba6fe37fae7e7b1c3b8cd0ab01e06b065ce1e76
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-21 11:00:20 +0900

    a

---
+++ java/Java_learning_Kubota/src/jp/co/scsk/kyushu/repetiton/Exercises02.java
@@ -0,0 +1,15 @@
+package jp.co.scsk.kyushu.repetiton;
+
+public class Exercises02 {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int i = 0;
+		while(i < 6) {
+			x = x * 2;
+			i = i + 1;
+		}
+		System.out.println(x);
+	}
+
+}
---
+++ java/Java_learning_Kubota/src/jp/co/scsk/kyushu/repetiton/Exercises04.java
@@ -0,0 +1,14 @@
+package jp.co.scsk.kyushu.repetiton;
+
+public class Exercises04 {
+	
+	public static void main(String[] args) {
+		int x = 100;
+		for (int n = 20; n >0; n++) {
+			x = x - n;
+			System.out.println(x);
+		}
+		System.out.println(x);
+	}
+	
+}
