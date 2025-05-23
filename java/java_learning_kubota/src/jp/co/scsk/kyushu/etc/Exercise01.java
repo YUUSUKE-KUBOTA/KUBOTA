608e6c8fb318112df1fcfc059ed23874ed3678d2
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-23 13:50:09 +0900

    a

---
+++ java/java_learning_kubota/src/jp/co/scsk/kyushu/etc/Exercise01.java
@@ -0,0 +1,22 @@
+package jp.co.scsk.kyushu.etc;
+
+public class Exercise01 {
+
+	public static double getValue(int age) {
+		double fee = 2000;
+		if (age < 6 || age > 100) {
+			fee = 0;
+		} else if (age > 60) {
+			fee = fee - (fee * (100 - age) / 100d);
+		} else if (age % 5 == 0) {
+			fee = fee - (fee * 0.05);
+		} else if (age % 3 == 0) {
+			fee = fee - (fee * 0.03);
+		} else if (age % 5 == 0 && age % 3 == 0) {
+			fee = fee - (fee * 0.1);
+		} else if (age < 20) {
+			fee = 800;
+		}
+		return fee;
+	}
+}
