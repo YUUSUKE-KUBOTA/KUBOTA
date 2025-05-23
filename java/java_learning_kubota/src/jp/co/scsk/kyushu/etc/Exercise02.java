9972e1f9d4d7d07a1c45865a4ff48d85a2955b97
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-23 14:17:39 +0900

    a

--- java/java_learning_kubota/src/jp/co/scsk/kyushu/etc/Exercise02.java
+++ java/java_learning_kubota/src/jp/co/scsk/kyushu/etc/Exercise02.java
@@ -1,17 +1,28 @@
 package jp.co.scsk.kyushu.etc;
 
 public class Exercise02 {
-	
+
 	public static void main(String[] args) {
-		int long = new create();
-		
+		int arraylong = 5;
+		int array[] = create(arraylong);
+		double arrayAvg = getAvg(array);
+		System.out.println(arrayAvg);
 	}
-	
-	public static int create() {
-		
+
+	public static int[] create(int arraylong) {
+		int[] array;
+		array = new int[arraylong];
+		for (int i = 0; i < arraylong; i++) {
+			array[i] = (int) (Math.random() * 100) + 1;
+		}
+		return array;
 	}
-	
-	public static double getAvg() {
-		
+
+	public static double getAvg(int array[]) {
+		double arrayAvg = 0;
+		for (int i = 0; i < array.length; i++) {
+			arrayAvg += array[i];
+		}
+		return arrayAvg / array.length;
 	}
 }
