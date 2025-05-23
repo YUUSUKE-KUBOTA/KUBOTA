90af3069b4e45111fb658abe64f9f01047b8dd08
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-23 10:02:11 +0900

    a

--- java/java_learning_kubota/src/jp/co/scsk/kyushu/collectionSample/GradesData02.java
+++ java/java_learning_kubota/src/jp/co/scsk/kyushu/collectionSample/GradesData02.java
@@ -5,41 +5,37 @@ import java.util.HashMap;
 
 public class GradesData02 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        HashMap<String, Integer> studentScore = new HashMap<>();
-        Data02 data02 = new Data02();
-        String[] names = data02.getNames();
-        int[] points = data02.getPoints();
+		HashMap<String, Integer> studentScore = new HashMap<>();
+		Data02 data02 = new Data02();
+		String[] names = data02.getNames();
+		int[] points = data02.getPoints();
 
-        for (int i = 0; i < names.length; i++) {
-            if (studentScore.containsKey(names[i])) {
-                if (studentScore.get(names[i]) < points[i]) {
-                    studentScore.put(names[i], points[i]);
-                }
-            } else {
-                studentScore.put(names[i], points[i]);
-            }
-        }
+		for (int i = 0; i < names.length; i++) {
+			if (studentScore.containsKey(names[i])) {
+				if (studentScore.get(names[i]) < points[i]) {
+					studentScore.put(names[i], points[i]);
+				}
+			} else {
+				studentScore.put(names[i], points[i]);
+			}
+		}
 
-        // キーをリストに変換
-        ArrayList<String> mapKeys = new ArrayList<>(studentScore.keySet());
-
-        // バブルソートアルゴリズムによるソート
-        for (int i = 0; i < mapKeys.size() - 1; i++) {
-            for (int j = 0; j < mapKeys.size() - 1 - i; j++) {
-                if (studentScore.get(mapKeys.get(j)) < studentScore.get(mapKeys.get(j + 1))) {
-                    // スコアが低い場合は位置を交換
-                    String temp = mapKeys.get(j);
-                    mapKeys.set(j, mapKeys.get(j + 1));
-                    mapKeys.set(j + 1, temp);
-                }
-            }
-        }
-
-        // 出力結果
-        for (String name : mapKeys) {
-            System.out.println(name + ": " + studentScore.get(name));
-        }
-    }
+		ArrayList<String> mapKeys = new ArrayList<>(studentScore.keySet());
+		for (int i = 0; i < mapKeys.size() - 1; i++) {
+			for (int j = 0; j < mapKeys.size() - 1 - i; j++) {
+				if (studentScore.get(mapKeys.get(j)) < studentScore.get(mapKeys.get(j + 1))) {
+					String temp = mapKeys.get(j);
+					mapKeys.remove(j);
+					mapKeys.add(j, mapKeys.get(j));
+					mapKeys.remove(j + 1);
+					mapKeys.add(j + 1, temp);
+				}
+			}
+		}
+		for (String name : mapKeys) {
+			System.out.println(name + ": " + studentScore.get(name));
+		}
+	}
 }
