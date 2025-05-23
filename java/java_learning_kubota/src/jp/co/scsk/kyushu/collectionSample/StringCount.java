068458d4525fe31833b1bd5766bd4d0bf2cecc2f
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-23 13:01:32 +0900

    a

--- java/java_learning_kubota/src/jp/co/scsk/kyushu/collectionSample/StringCount.java
+++ java/java_learning_kubota/src/jp/co/scsk/kyushu/collectionSample/StringCount.java
@@ -1,11 +1,25 @@
 package jp.co.scsk.kyushu.collectionSample;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StringCount {
 	public static void main(String[] args) {
 		InputUtil inputData = new InputUtil();
-		ArrayList<String> array = new ArrayList<>();
+		String data = inputData.input();
+		String[] splitData = data.split("");
+		HashMap<String, Integer> countStr = new HashMap<>();
 
+		for (int i = 0; i < splitData.length; i++) {
+			String charStr = splitData[i];
+			if (!countStr.containsKey(charStr)) {
+				countStr.put(charStr, 1);
+			} else {
+				countStr.put(charStr, countStr.get(charStr) + 1);
+			}
+		}
+
+		for (String charStr : countStr.keySet()) {
+			System.out.println(charStr + ":" + countStr.get(charStr));
+		}
 	}
 }
