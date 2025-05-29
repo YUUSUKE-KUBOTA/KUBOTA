558bddf61ec427f90e358d1e0000f0912ab6da9e
kubota <Yusuke_Kubota@scsk-kyushu.co.jp>
2025-05-29 15:50:39 +0900

    a

--- java/java_learning_kubota/src/jp/co/scsk/kyushu/exBasic/MyArray.java
+++ java/java_learning_kubota/src/jp/co/scsk/kyushu/exBasic/MyArray.java
@@ -5,7 +5,7 @@ public class MyArray {
 	public int[] getRomdomNumberArray(int num) {
 		int[] array;
 		array = new int[num];
-		for (int i = 0; i <= num; i++) {
+		for (int i = 0; i < num; i++) {
 			array[i] = (int) (Math.random() * 101);
 		}
 		return array;
